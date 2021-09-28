c91c7557d67e7ae99f57d7e056ca1984d0e12023
umapalam <umapalam@gmail.com>
2021-09-28 11:49:48 -0400

    Added Database Connection

--- server.js
+++ server.js
@@ -2,16 +2,36 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+const mongoose = require('mongoose')
+
+//DATABASE CONNECTION - LOCALHOST
+const mongoURI = "mongodb://127.0.0.1:27017/foreignfilms"
+const db = mongoose.connection
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, ()=> {
+      console.log('database connected')
+})
+
+db.on('error', (err) => {console.log('ERROR: ', err) })
+db.on('connected', () => {console.log('mongo connected')})
+db.on('disconnected', () =>{console.log('mongo disconnected')})
+
+//MIDDLEWARE
+app.use(express.urlencoded({ extended: true }))
+
+//CONTROLLERS 
+
 app.get('/', (req, res) => {
     res.send("This Works!")
 })
-
-//CONTROLLERS 
 
 // NEW
 app.get('/films/new', (req, res) => {
     res.render('new.ejs')
 })
+
 // INDEX
 app.get('/films', (req, res) => {
     res.send("This Route is Working!")
